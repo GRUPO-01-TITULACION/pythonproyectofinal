18cf1ac2c9ed3894aeefda73257e047753adb32d
GRUPO-01-TITULACION <titulacionuniandes25@gmail.com>
2025-05-20 23:08:33 -0500

    MOdelo participantes

--- capacita_carchi/carchiapp/models.py
+++ capacita_carchi/carchiapp/models.py
@@ -1,3 +1,28 @@
-from django.db import models
 
-# Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Participante(models.Model):
+    cedula = models.CharField(max_length=10, unique=True)
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    genero = models.CharField(max_length=15, blank=True, null=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nombres} {self.apellidos}"
